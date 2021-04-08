5de17726aeea76bf050a5b1b19da9bd829d6307e
olena.mordas <olena.mordas@synpulse.com>
2021-04-08 18:10:47 +0200

    Initial commit & simple Rest Controller

---
+++ src/main/java/com/example/bootdemo/Book.java
@@ -0,0 +1,35 @@
+package com.example.bootdemo;
+
+public class Book {
+
+    long id;
+    String name;
+    String author;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Book(long id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+}
